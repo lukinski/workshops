4190e9abdeb23a1c7c35da14b613d65a9478fa80
Lukasz Modlinski <lukasz@extraweb.pl>
2014-09-11 20:23:55 +0200

    Add Product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :description, :title, :price
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
 end
